5e93a0b3f291e4351f49031d143f942bf410957e
NSmocker <sinchik97@gmail.com>
2023-12-07 00:05:14 +0200

    123

--- PWSnowballs/Assets/PlayerController.cs
+++ PWSnowballs/Assets/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
 	public Vector2 move_vector;
 	public Vector2 camera_vector;
 	
+	
+	
 	// Start is called before the first frame update
     void Start()
     {
