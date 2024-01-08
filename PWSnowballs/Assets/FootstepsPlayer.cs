a216901cc65264b6333799d28afdd8e60b167578
NSmocker <sinchik97@gmail.com>
2024-01-08 19:28:24 +0200

    123

---
+++ PWSnowballs/Assets/FootstepsPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepsPlayer : MonoBehaviour
+{
+	public AudioClip[] footsteps;
+	public int footstep_id;
+	public AudioSource sound_source;
+	
+	public void PlayFootstepSound()
+	{
+		if(footstep_id>1)footstep_id=0;
+		sound_source.PlayOneShot(footsteps[footstep_id]);
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
