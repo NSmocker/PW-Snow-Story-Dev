3688b84c5b09c65400d3ef9efbe8ff128cd2749d
NSmocker <sinchik97@gmail.com>
2024-01-18 19:21:43 +0200

    Walk animation

---
+++ PWSnowballs/Assets/Scripts/AnimationSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSystem : MonoBehaviour
+{
+	public CharacterMovement movement_system;
+	public Animator anim_link;
+	public float movement_magnitude;
+	public float sprint;
+ 
+ 
+	public void AnimateByInput(Vector2 move_info)
+	{
+		movement_magnitude = move_info.magnitude;
+	
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    anim_link.SetFloat("movement_magnitude",movement_magnitude);
+	    // anim_link.SetFloat("sprint",sprint);
+	    
+    }
+}
